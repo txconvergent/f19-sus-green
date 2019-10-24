7c6cefa5eca2c7219b32524316362dce839dbb51
Jin Lee <jinolemo@gmail.com>
2019-10-23 20:04:09 -0500

    initial code

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api_call', (req, res) => res.send({message: 'Hello World!'}))
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
