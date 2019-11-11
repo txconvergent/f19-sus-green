9f57c49ac188633b906291ed49b1b9317e1fc638
alyssajlin <41011614+alyssajlin@users.noreply.github.com>
2019-11-10 18:33:40 -0600

    adding GridView to second page
    added gridview to the secondary page, which can eventually represent lots. includes experimenting w different types of grids

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(App());
 class App extends StatelessWidget {
   @override
@@ -47,7 +48,7 @@ class FirstScreen extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Map Page"),
       ),
-      body: new Text("Something else"),
+      body: grid(),
     );
   }
 }
@@ -60,5 +61,65 @@ class FirstScreen extends StatelessWidget {
             ));
       }
   
-  
-  
+  class grid1 extends StatelessWidget{
+    Widget build (BuildContext context){
+      return new GridView.count(
+  // Create a grid with 2 columns. If you change the scrollDirection to
+  // horizontal, this produces 2 rows.
+  crossAxisCount: 2,
+  // Generate 100 widgets that display their index in the List.
+  children: List.generate(100, (index) {
+    return Center(
+      child: Text(
+        'Item $index',
+        //style: Theme.of(context).textTheme.headline,
+      ),
+    );
+  }),
+);
+    }
+  }
+
+  class grid extends StatelessWidget{
+  Widget build (BuildContext ctxt) {
+  return new GridView.count(
+  primary: false,
+  padding: const EdgeInsets.all(20),
+  crossAxisSpacing: 100,
+  mainAxisSpacing: 10,
+  crossAxisCount: 2,
+  children: <Widget>[
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: grid1(),
+      color: Colors.teal[100],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Heed not the rabble'),
+      color: Colors.teal[200],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Sound of screams but the'),
+      color: Colors.teal[300],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Who scream'),
+      color: Colors.teal[400],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution is coming...'),
+      color: Colors.teal[500],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.teal[600],
+    ),
+  ],
+);
+  }
+  }
